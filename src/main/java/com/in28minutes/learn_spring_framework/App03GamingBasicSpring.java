71792dc5038f1a60469fd1af1722404ad6a6a26a
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-13 11:18:34 +0700

    updated

--- src/main/java/com/in28minutes/learn_spring_framework/App03GamingBasicSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App03GamingBasicSpring.java
@@ -1,17 +1,34 @@
 package com.in28minutes.learn_spring_framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.in28minutes.learn_spring_framework.games.GameConsole;
 import com.in28minutes.learn_spring_framework.games.GameRunner;
+import com.in28minutes.learn_spring_framework.games.PacmanGame;
 
+@Configuration
 public class App03GamingBasicSpring {
+	@Bean
+	public GameConsole pacmanGame() {
+		GameConsole pacman = new PacmanGame();
+		return pacman;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GameConsole game) {
+		GameRunner gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		AnnotationConfigApplicationContext context = null;
 		try {			
 			context = new AnnotationConfigApplicationContext
-					(GamingConsoleConfiguration.class);
+					(App03GamingBasicSpring.class);
 			
 			var gameRunner = context.getBean(GameRunner.class);
 			gameRunner.run();
